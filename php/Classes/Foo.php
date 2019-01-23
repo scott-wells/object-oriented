edc67fedbc4dc671359f10cb423fda9db4bd817b
Scott Wells <efwells101@gmail.com>
2019-01-23 13:56:31 -0700

    add docblocks for properties add constructor method

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -2,69 +2,108 @@
 
 class Author {
 
-	public $authorId;
-	public $authorAvatarUrl;
-	public $authorActivationToken;
-	public $authorEmail;
-	public $authorHash;
-	public $authorUsername;
+	use ValidateDate;
+	use ValidateUuid;
+	/**
+	 * id for this Author; this is the primary key
+	 * @var Uuid $authorId
+	 **/
+	private $authorId;
+	/**
+	 * url for the author's avatar photo; this is a string
+	 * @var $authorAvatarUrl
+	 **/
+	private $authorAvatarUrl;
+	/**
+	 * temporary password sent to author's email for validation; this is a string
+	 * @var $authorActivationToken
+	 **/
+	private $authorActivationToken;
+	/**
+	 * author's email; this is a string with a special character
+	 * @var $authorEmail
+	 **/
+	private $authorEmail;
+	/**
+	 * returns hashed characters for author's password; this is a string
+	 * @var $authorHash
+	 **/
+	private $authorHash;
+	/**
+	 * author's username; this is a string
+	 * @var $authorUsername
+	 **/
+	private $authorUsername;
 
+	/**
+	 * @param string|Uuid $authorId id of this Author
+	 * @param string $authorAvatarUrl url for the author's avatar photo
+	 * @param string $authorActivationToken temporary password sent to author's email for validation
+	 * @param string $authorEmail author's email
+	 * @param string $authorHash returns hashed characters for author's password
+	 * @param string $authorUsername author's username
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g. strings too long, negative integers, etc)
+	 * @throws \TypeError if data type violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+//	public function __construct($authorId, string $authorAvatarUrl, $authorActivationToken, string $authorEmail, $authorHash, string $authorUsername) {
+//		try {
+//			$this->setAuthorId($authorId);
+//		}
+//	}
 
-
-	public function setAuthorId($authorId) {
+	private function setAuthorId($authorId) {
 		$this->authorId = $authorId;
 	}
 
-	public function getAuthorId() {
+	private function getAuthorId() {
 		return $this->authorId;
 	}
 
 
-	public function setAuthorAvatarUrl($authorAvatarURL) {
+	private function setAuthorAvatarUrl($authorAvatarURL) {
 		$this->authorAvatarUrl = $authorAvatarURL;
 	}
 
-	public function getAuthorAvatarUrl() {
+	private function getAuthorAvatarUrl() {
 		return $this->authorAvatarUrl;
 	}
 
 
-	public function setAuthorActivationToken($authorActivationToken) {
+	private function setAuthorActivationToken($authorActivationToken) {
 		$this->authorActivationToken = $authorActivationToken;
 	}
 
-	public function getAuthorActivationToken() {
+	private function getAuthorActivationToken() {
 		return $this->authorActivationToken;
 	}
 
 
-	public function setAuthorEmail($authorEmail) {
+	private function setAuthorEmail($authorEmail) {
 		$this->authorEmail = $authorEmail;
 	}
 
-	public function getAuthorEmail() {
+	private function getAuthorEmail() {
 		return $this->authorEmail;
 	}
 
 
-	public function setAuthorHash($authorHash) {
+	private function setAuthorHash($authorHash) {
 		$this->authorHash = $authorHash;
 	}
 
-	public function getAuthorHash() {
+	private function getAuthorHash() {
 		return $this->authorHash;
 	}
 
 
-	public function setAuthorUsername($authorUsername) {
+	private function setAuthorUsername($authorUsername) {
 		$this->authorUsername = $authorUsername;
 	}
 
-	public function getAuthorUsername() {
+	private function getAuthorUsername() {
 		return $this->authorUsername;
 	}
 }
 
-$author1 = new Author();
-$author1->authorUsername = "Will Smith";
-$author1->authorEmail = "email04@email.com";
---
+++ php/lib/foo.php
@@ -0,0 +1,5 @@
+<?php
+
+//$author1 = new Author();
+//$author1->authorUsername = "Will Smith";
+//$author1->authorEmail = "email04@email.com";
