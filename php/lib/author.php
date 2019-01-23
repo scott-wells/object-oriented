8c4920cab7db9b10a6eda2033247c4cd6bbc4a7c
Scott Wells <efwells101@gmail.com>
2019-01-23 16:43:54 -0700

    change file names add namespace and ramsey uuid

---
+++ php/lib/author.php
@@ -0,0 +1,6 @@
+<?php
+namespace ScottWells\ObjectOriented;
+
+use Ramsey\Uuid\Uuid;
+
+$newAuthor = new Author("d7e3bc4e437c437cb945b155a8ddf0c7", "https://www.author.picture.com", "nananananananananananananananana", "email05@email.com", "nanananananananananananananananananananananananananananananananananananananananananananananananaa", "Seemore Butts");
--- php/lib/foo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-//$author1 = new Author();
-//$author1->authorUsername = "Will Smith";
-//$author1->authorEmail = "email04@email.com";
